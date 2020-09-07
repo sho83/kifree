63de20f5294fa09a6bfc375e72ccd71bbdd7902e
Panrise <70122578+Panrise@users.noreply.github.com>
2020-09-07 23:20:05 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 $(function(){
   $('a[href^="#"]').click(function(){
     var speed = 500;
+    var adjust=0;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
+    $("html, body").animate({scrollTop:position}, 1500, "swing");
     return false;
   });
 });
