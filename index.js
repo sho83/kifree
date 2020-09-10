f3a724e271538f9aae6658424b635605b109487e
Panrise <70122578+Panrise@users.noreply.github.com>
2020-09-10 23:01:29 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-$(function(){
-  $('a[href^="#"]').click(function(){
-    var speed = 500;
-    var adjust=0;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, 1500, "swing");
-    return false;
-  });
-});
 
 $(document).on("click", "#abcd", function(){
   $("html,body").animate({scrollTop:$('#home').offset().top},1000,'swing');
                           
 });
+
+$(function(){
+        
+        $('a[href^="#"]').click(function() {
+          var adjust=0;
+          var speed = 500; 
+          var href= $(this).attr("href");
+          var target = $(href == "#" || href == "" ? 'html' : href);
+          var position = target.offset().top;
+          $('body,html').animate({scrollTop:position}, 1000, 'swing');
+          return false;
+        });
+      });
